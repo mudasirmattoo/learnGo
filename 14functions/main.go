891c1abd625dd01985ba4189253c19c92d0b16b7
mudasirmattoo <mudasirmmattoo@gmail.com>
2025-02-09 15:52:21 +0530

    variadic functions, unfurling slices

--- 14functions/main.go
+++ 14functions/main.go
@@ -22,12 +22,24 @@ func main() {
 	proadder, mymessage := proAdder(2, 9, 20, 1, 4, 5, 7)
 	fmt.Println("pro result is: ", proadder, mymessage)
 
+	name := muda("Muda")
+	fmt.Println(name)
+
+	x := variadicSum(1, 2, 3, 45, 67, 7)
+	fmt.Println(x)
+
+	//unfurling slice
+	xi := []int{2, 4, 5, 67, 2, 1}
+	hey, msg := proAdder(xi...)
+	fmt.Println(hey, msg)
+
 }
 
 func adder(val1 int, val2 int) int {
 	return val1 + val2
 }
 
+//variadic functions
 func proAdder(values ...int) (int, string) {
 	total := 0
 	for _, val := range values {
@@ -36,6 +48,21 @@ func proAdder(values ...int) (int, string) {
 	return total, "hey profunction"
 }
 
+func variadicSum(sliceofInt ...int) int {
+	fmt.Println(sliceofInt)
+
+	sum := 0
+
+	for _, v := range sliceofInt {
+		sum += v
+	}
+	return sum
+}
+
 func greetings() {
 	fmt.Println("hello there")
+}
+
+func muda(s string) string {
+	return fmt.Sprint("they call me ", s)
 }
